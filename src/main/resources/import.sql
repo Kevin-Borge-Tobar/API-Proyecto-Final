0c90dd9175d384ee502e61534648b4c1986202db
Kevin Borge <kevinborgetobar1998@gmail.com>
2022-06-02 06:00:00 -0400

    se realizo configuracion en el java y jdk

--- src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-/* Populate tables */
-INSERT INTO clientes ( nit, nombre, apellido,  fecha_creacion,email,telefono) VALUES('103456789','Kevin', 'Borge', 'kborget@miumg.edu.gt', '2022-08-01', '');
-INSERT INTO clientes ( nit, nombre, apellido,  fecha_creacion,email,telefono) VALUES('103456789','Miguel', 'Lopez', 'miguel.lopez@gmail.com', '2022-08-02', '');
-INSERT INTO clientes ( nit, nombre, apellido,  fecha_creacion,email,telefono) VALUES('103456789','Linus', 'Torvalds', 'linus.torvalds@gmail.com', '2022-08-03', '');
