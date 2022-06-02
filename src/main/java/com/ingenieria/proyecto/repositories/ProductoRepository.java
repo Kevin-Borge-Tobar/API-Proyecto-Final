235153318bb8de29d54412a5bddfd817df88d3b5
Kevin Borge <kevinborgetobar1998@gmail.com>
2022-06-02 04:26:29 -0400

    Inicio de la Api del proyecto final

--- src/main/java/com/ingenieria/proyecto/repositories/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ingenieria.proyecto.repositories;
-
-import com.ingenieria.proyecto.entities.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository  extends JpaRepository<Producto, Long> {
-}
